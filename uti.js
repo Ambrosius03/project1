cee2e11cb61516d80220e306521113fd75c993d4
Ambrosius03 <148223385+Ambrosius03@users.noreply.github.com>
2024-02-29 23:13:54 +0700

    Add files via upload

---
+++ uti.js
@@ -0,0 +1,17 @@
+function showSidebar(){
+    const sidebar = document.getElementById("close").style.width ="250px";
+}
+
+function hideSidebar(){
+    document.getElementById("close").style.width = "0";
+}
+
+//function openNav() {
+    //document.getElementById("close").style.width = "250px";
+   //document.getElementById("second").style.marginLeft = "250px";
+  //}
+  
+ // function closeNav() {
+   // document.getElementById("close").style.width = "0";
+   // document.getElementById("second").style.marginLeft= "0";
+  //}
